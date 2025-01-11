54628d87e15e19fd852eb46bf5b7d5fb18ad20b7
sam-sny <sampsonekpo6@gmail.com>
2025-01-11 21:53:29 +0100

    second commit

---
+++ app/schemas/schema.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, EmailStr
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str
+
+class UserResponse(UserBase):
+    id: int
+    is_active: bool
+    is_google_user: bool
+
+    class Config:
+        orm_mode = True
